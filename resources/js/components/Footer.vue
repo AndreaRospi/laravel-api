400707f46130a88522eabeb345df25b7a3dbd3b9
Andrea <andr3z45@gmail.com>
2022-02-24 02:12:20 +0100

    aggiunta vue

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <h2>Sono Footer</h2>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h2>Sono Header</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+</style>
